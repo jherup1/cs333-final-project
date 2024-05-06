0fb1b224aedaa7b77d5065e771de13408849103d
jherup1 <joshuaherup@gmail.com>
2024-05-06 13:33:40 -0700

    Created script versions for linux and windows

--- PULL & CREATE CONTAINER - Linux.sh
+++ PULL & CREATE CONTAINER - Linux.sh
@@ -1,4 +1,4 @@
 docker pull jherup22/cs333-final-project
-docker rm flashcard_app
+docker rm -f flashcard_app
 docker run --name flashcard_app -itd jherup22/cs333-final-project
 docker stop flashcard_app
